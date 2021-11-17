19d21aa449036326ce837abfdf54377869a993f8
Pawan Janglu Lode <pawanjlode@gmail.com>
2021-11-17 22:18:45 +0530

    testing

---
+++ testing/question2/validator.js
@@ -0,0 +1,12 @@
+function validatePass(pass){
+    let symbol = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ',', '.', '<', '>', '/', ';', ':', ']', '[', '{', '}', `\\`, '|', '?'];
+    let smallLetters = abcdefghijklmnopqrstuvwxyz;
+    let capLetters = ABCDEFGHIJKLMNOPQRSTUVWXYZ;
+    let num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let lettercount = 0;
+    let numcount = 0;
+    let numofSymbols = 0;
+    for (let i = 0; i < pass.length; i++){
+        if()
+    }
+}

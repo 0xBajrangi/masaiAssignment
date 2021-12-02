d3b6e4187a9d875c45ca531620aef300f5f6fc0b
Pawan Janglu Lode <pawanjlode@gmail.com>
2021-12-03 00:37:24 +0530

    completed

--- unitIV/coding Assignment/1-12/src/controller/user.controller.js
+++ unitIV/coding Assignment/1-12/src/controller/user.controller.js
@@ -14,7 +14,8 @@ const upload = require('../middleware/upload');
 
 router.get('/', async (req, res) => {
     const user = await User.find().lean().exec();
-    return res.send(user)
+    const gallery = await Gallery.find().lean().exec();
+    return res.send({user,gallery})
 })
 
 router.post('/profile', upload.single("profile"), async (req, res) => {
@@ -70,10 +71,10 @@ router.delete('/user/:id', async (req, res) => {
     try {
         
         let user = await User.findByIdAndDelete(req.params.id);
-        
+
         fs.unlink(user.profile_pic, (err) => {
             if (err) {
-                throw errl;
+                throw err;
             }
             console.log("file deleted");
         })
@@ -84,6 +85,60 @@ router.delete('/user/:id', async (req, res) => {
         res.status(500).json({ message: e.message });
     }
 });
+router.delete('/gallary/:id', async (req, res) => {
+    try {
+
+        
+        let gallary = await Gallery.findByIdAndDelete(req.params.id);
+        console.log(gallary.pictures);
+        gallary.pictures.forEach((el) => {
+            
+            fs.unlink(el, (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log("file deleted");
+            })
+        });
+        console.log(await Gallery.find())
+
+      
+        return res.status(200).json({ gallary });
+    } catch (e) {
+        res.status(500).json({ message: e.message });
+    }
+});
+
+router.patch('/:id',upload.single("profile"),async (req, res )=> {
+    
+
+    try {
+
+        let curr = await User.findById(req.params.id);
+
+        fs.unlink(curr.profile_pic, (err) => {
+            if (err) {
+                throw err;
+            }
+            else {
+                console.log(curr.profile_pic, "deleted")
+            }
+        });
+
+        let user = await User.findByIdAndUpdate(req.params.id, {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            profile_pic: req.file.path,
+        });
+    
+      
+      
+        return res.status(200).json({ user });
+    } catch (e) {
+        res.status(500).json({ message: e.message });
+    }
+
+})
 
 
 
